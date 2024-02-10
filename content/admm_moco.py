8ff007558c6238e2822971f5f64a9feb6b3196d0
Georg Schramm <georg.schramm@kuleuven.be>
2024-02-10 13:58:38 +0100

    add quad. reg. and cost function

--- content/admm_moco.py
+++ content/admm_moco.py
@@ -1,33 +1,68 @@
 """ minimal example of ADMM for motion correction 
     simple 1D "denoising" problem with 3 shifted signals
     solutions to the subproblems can be computed analytically
+    (quadratic regularization)
 
-TODO: use simple diagonal operator A (multiplication with triangle profile)
+Learnings:
+1. align "z to referenece z" to estimate motion
+2. in the first iterations, rho controls whether the z's are close 
+to the individual reconstructions or close to the average
+-> to be able to estimate motion, we need "small" rho (e.g. 0.1)
+-> rho too small also suboptimal, 
+   because then the z's are too close to the individual reconstructions
 """
 
+# %%
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.ndimage import gaussian_filter1d
+
+
+def cost_function(
+    recon: np.ndarray,
+    diag_fwd_op: np.ndarray,
+    shifts: list[int],
+    d: list[np.ndarray],
+    bet: float,
+) -> float:
+
+    data_fidelities = np.zeros(len(shifts))
+
+    for k, s in enumerate(shifts):
+        exp_data = diag_fwd_op * np.roll(recon, s)
+        data_fidelities[k] = 0.5 * ((exp_data - d[k]) ** 2).sum()
+
+    recon_grad = np.roll(recon, -1) - recon
+    prior = 0.5 * bet * (recon_grad**2).sum()
+
+    return data_fidelities.sum() + prior
+
 
 # %%
 # input parameters
-np.random.seed(2)
+np.random.seed(1)
 
 n = 201
 noise_level = 0.1
+num_iter = 1000
+
+# very big rho means that information between z is heavily shared (z more lambda like)
+# -> not good when we want to align the z's to get a motion update
+# very small row means that the z's stay very close to the ind. recons of the data
+# which is better for motion estimation, but noise gets a problem
+# -> there should be a sweet spot for rho, here this is around 1e-1
 rho = 1e-1
-num_iter = 100
+
 
 # true shifts for the 3 gates
-s1 = (n // 10) % n
+s1 = (n // 4) % n
 s2 = (-n // 5) % n
 s3 = 0
 
 # errors for the shifts during recon
 # 0 means use of true shifts during recon
-s1_error = 0  # + n // 16  # set to -s1 for no shift modeling during recon
-s2_error = 0  # + n // 16  # set to -s2 for no shift modeling during recon
-s3_error = 0  #  # set to -s3 for no shift modeling during recon
+s1_error = -s1  # 0 + n // 8  # set to -s1 for no shift modeling during recon
+s2_error = -s2  # 0 + n // 16  # set to -s2 for no shift modeling during recon
+s3_error = -s3  # 0  #  # set to -s3 for no shift modeling during recon
 
 # x coordinate
 x = np.linspace(-n // 2, n // 2, n)
@@ -35,10 +70,10 @@ x = np.linspace(-n // 2, n // 2, n)
 # this can be inverted / transposed easily
 diag_A = 1.2 * (x.max() - np.abs(x)) / x.max() + 0.1
 
-# sigma for Gaussian filter to emulate regularization
-sig_lam = 0.0
+# weight of quad. reg.
+beta = 1e1
 
-motion_update_period = 100000000
+motion_update_period = 1
 # %%
 
 # setup the true object
@@ -55,6 +90,41 @@ d1 = diag_A * f1 + np.random.normal(0, noise_level, n)
 d2 = diag_A * f2 + np.random.normal(0, noise_level, n)
 d3 = diag_A * f3 + np.random.normal(0, noise_level, n)
 
+
+# %%
+# setup the operator we need to analytically solve the ind. recons
+# argmin_x (beta/2) || grad x||^2 + 1/2 ||x - d||^2
+
+op = np.zeros((n, n))
+i = np.arange(n)
+
+op[i, i] = diag_A[i] ** 2 + 2 * beta
+op[i, (i - 1) % n] = -beta
+op[i, (i + 1) % n] = -beta
+
+op_inv = np.linalg.inv(op)
+
+# calculate the individual reconstructions
+r1 = op_inv @ (diag_A * d1)
+r2 = op_inv @ (diag_A * d2)
+r3 = op_inv @ (diag_A * d3)
+
+
+# %%
+# setup the operator we need to analytically solve subproblem 2
+# argmin_lam (beta/2) || grad lambda||^2 + \sum_k rho/2 ||S_k lambda - (z_k + u_k)||^2
+
+num_gates = 3
+
+op2 = np.zeros((n, n))
+
+op2[i, i] = rho * num_gates + 2 * beta
+op2[i, (i - 1) % n] = -beta
+op2[i, (i + 1) % n] = -beta
+
+op2_inv = np.linalg.inv(op2)
+
+
 # %%
 # ADMM recons
 
@@ -68,13 +138,14 @@ sr2s = [sr2]
 sr2s = [sr3]
 
 # init variables
-lam = (np.roll(d1, -sr1) + np.roll(d2, -sr2) + np.roll(d3, -sr3)) / 3
-# lam = np.zeros(n)
-# lam = f
+# lam = (np.roll(r1, -sr1) + np.roll(r2, -sr2) + np.roll(r3, -sr3)) / 3
+lam = np.zeros(n)
 
 u1 = np.zeros(n)
 u2 = np.zeros(n)
 u3 = np.zeros(n)
+
+cost = np.zeros(num_iter)
 
 for i in range(num_iter):
     # 1st sub-problem
@@ -87,17 +158,19 @@ for i in range(num_iter):
     z2 = (diag_A * d2 + rho * e2) / (diag_A**2 + rho)
     z3 = (diag_A * d3 + rho * e3) / (diag_A**2 + rho)
 
-    # sub-problem 2
-    lam = (np.roll(z1 + u1, -sr1) + np.roll(z2 + u2, -sr2) + np.roll(z3 + u3, -sr3)) / 3
+    # sub-problem 2 - analytic solution (possible if there is no regularization on lambda)
+    # lam = (np.roll(z1 + u1, -sr1) + np.roll(z2 + u2, -sr2) + np.roll(z3 + u3, -sr3)) / 3
 
-    #### HACK to emulate regularization on lambda -> not at all correct!
-    # if sig_lam > 0:
-    #    lam = gaussian_filter1d(lam, 5.0, mode="wrap")
+    v = rho * (np.roll(z1 + u1, -sr1) + np.roll(z2 + u2, -sr2) + np.roll(z3 + u3, -sr3))
+    lam = op2_inv @ v
 
     # update of u
     u1 = u1 + z1 - np.roll(lam, sr1)
     u2 = u2 + z2 - np.roll(lam, sr2)
     u3 = u3 + z3 - np.roll(lam, sr3)
+
+    # calculate the cost function
+    cost[i] = cost_function(lam, diag_A, [sr1, sr2, sr3], [d1, d2, d3], beta)
 
     # update the shifts
     if (i + 1) % motion_update_period == 0:
@@ -112,27 +185,27 @@ for i in range(num_iter):
 fig, ax = plt.subplots(4, 3, figsize=(16, 8), tight_layout=True)
 ax[0, 0].plot(x, f, "k", label="ground truth")
 ax[0, 0].plot(x, lam, "r", label=r"$\lambda$")
-ax[0, 0].set_title(f"rho={rho:.1e}, n={num_iter}", fontsize="medium")
+ax[0, 0].set_title(f"beta={beta:.1e}, rho={rho:.1e}, n={num_iter}", fontsize="medium")
 
 ax[1, 0].plot(x, d1, "k", label=r"$d_1$")
-ax[1, 0].plot(x, d1 / diag_A, "b", label=r"$A^{-1} d_1$")
+ax[1, 0].plot(x, r1, "b", label=r"$r_1$")
 ax[1, 0].plot(x, z1, "r", label=r"$z_1$")
 
 ax[2, 0].plot(x, d2, "k", label=r"$d_2$")
-ax[2, 0].plot(x, d2 / diag_A, "b", label=r"$A^{-1} d_2$")
+ax[2, 0].plot(x, r2, "b", label=r"$r_2$")
 ax[2, 0].plot(x, z2, "r", label=r"$z_2$")
 ax[3, 0].plot(x, d3, "k", label=r"$d_3$")
-ax[3, 0].plot(x, d3 / diag_A, "b", label=r"$A^{-1} d_3$")
+ax[3, 0].plot(x, r3, "b", label=r"$r_3$")
 ax[3, 0].plot(x, z3, "r", label=r"$z_3$")
 
 ax[1, 1].plot(x, d1, "k", label=r"$d_1$")
-ax[1, 1].plot(x, d1 / diag_A, "b", label=r"$A^{-1} d_1$")
+ax[1, 1].plot(x, r1, "b", label=r"$r_1$")
 ax[1, 1].plot(x, z1 + rho * u1, "r", label=r"$z_1 + \rho u_1$")
 ax[2, 1].plot(x, d2, "k", label=r"$d_2$")
-ax[2, 1].plot(x, d2 / diag_A, "b", label=r"$A^{-1} d_2$")
+ax[2, 1].plot(x, r2, "b", label=r"$r_2$")
 ax[2, 1].plot(x, z2 + rho * u2, "r", label=r"$z_2 + \rho u_2$")
 ax[3, 1].plot(x, d3, "k", label=r"$d_3$")
-ax[3, 1].plot(x, d3 / diag_A, "b", label=r"$A^{-1} d_3$")
+ax[3, 1].plot(x, r3, "b", label=r"$r_3$")
 ax[3, 1].plot(x, z3 + rho * u3, "r", label=r"$z_3 + \rho u_3$")
 
 ax[1, 2].plot(x, d1, "k", label=r"$d_1$")
@@ -147,9 +220,6 @@ for axx in ax[1:, :].ravel():
     axx.grid(ls=":")
 ax[0, 0].legend()
 ax[0, 0].grid(ls=":")
-
-# ymin = min([axx.get_ylim()[0] for axx in ax[:, :-1].ravel()])
-# ymax = max([axx.get_ylim()[1] for axx in ax[:, :-1].ravel()])
 
 ymin = d3.min()
 ymax = d3.max()
@@ -173,3 +243,13 @@ for axx in ax[0, 1:]:
     axx.axhline(s2, color=plt.cm.tab10(1), ls="--")
 
 fig.show()
+
+# %%
+# plot the cost function
+fig2, ax2 = plt.subplots(tight_layout=True)
+ax2.semilogx(np.arange(1, num_iter + 1), cost)
+ax2.set_title("cost function")
+ax2.grid(ls=":")
+fig2.show()
+
+print(f"rho: {rho:.1e}, num_iter: {num_iter}, cost: {cost[-1]:.4e}")
